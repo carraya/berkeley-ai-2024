d1ba50b8a05c509ca8f99df3f0e934bf2b3a2263
Taha Ansari <tahaaansari@hotmail.com>
2024-06-23 03:01:27 -0700

    change .environ to .getenv

--- app/main.py
+++ app/main.py
@@ -24,14 +24,14 @@ app = FastAPI()
 default_app = firebase_admin.initialize_app(credential=credentials.Certificate({
     "type": "service_account",
     "project_id": "berkeley2024-d8b6a",
-    "private_key_id": os.environ["PRIVATE_KEY_ID"],
-    "private_key": os.environ["PRIVATE_KEY"],
-    "client_email": os.environ["CLIENT_EMAIL"],
-    "client_id": os.environ["CLIENT_ID"],
+    "private_key_id": os.getenv("PRIVATE_KEY_ID"),
+    "private_key": os.getenv("PRIVATE_KEY"),
+    "client_email": os.getenv("CLIENT_EMAIL"),
+    "client_id": os.getenv("CLIENT_ID"),
     "auth_uri": "https://accounts.google.com/o/oauth2/auth",
     "token_uri": "https://oauth2.googleapis.com/token",
     "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": os.environ["CLIENT_CERT_URL"],
+    "client_x509_cert_url": os.getenv("CLIENT_CERT_URL"),
     "universe_domain": "googleapis.com"
 }))
 db = firestore.client()
